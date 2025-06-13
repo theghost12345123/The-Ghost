5fbcf75c71e37e5fa889584b50647de0d76abd5e
theghost12345123 <ghostbot683@gmail.com>
2025-06-13 10:37:39 +0600

    Create 😂.js

---
+++ scripts/cmds/😂.js
@@ -0,0 +1,31 @@
+module.exports = {
+  config: {
+    name: "😂",
+    version: "0.0.1",
+    author: "ArYAN",
+    description: "Reply with audio when message starts with 😒",
+    usage: "😂",
+    cooldown: 5,
+    role: 0,
+    noPrefix: true
+  },
+
+  onStart: async () => {},
+
+  onChat: async ({ event, message }) => {
+    const text = event.body?.trim();
+    if (!text || !text.startsWith("😂")) return;
+
+    const audioUrl = "http://aryan-xy.onrender.com/raw/n-6_nUMt.mp3";
+
+    try {
+      await message.reply({
+        body: "",
+        attachment: await global.utils.getStreamFromURL(audioUrl)
+      });
+      message.react("😁");
+    } catch (err) {
+      console.error(err);
+    }
+  }
+};
